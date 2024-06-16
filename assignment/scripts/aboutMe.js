d287d62906257e7d7325c3e518f622709d676d0e
Tom Pinataro <tompinataro@gmail.com>
2024-06-16 00:04:26 -0500

    last attempt

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -19,7 +19,7 @@ console.log(luckyNumber);
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
-let introduction = 'My name is fullName, and I think luckyNumber is a winner!';
+let introduction = `My name is ${fullName}, and I think ${luckyNumber} is a winner!`;
 console.log(introduction);
 
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
